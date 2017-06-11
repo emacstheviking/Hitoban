a99aa3e681f53ba541aa2a72c94266e16c4e3b02
Loodoor <folaefolc@outlook.fr>
2017-06-11 21:58:38 +0200

    updating makefile

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ echo "Environment: `uname -a`"
 echo "Compiler: `$CXX --version`"
 
 make || exit 1
-make test || exit 1
---
+++ makemakefile.bat
@@ -0,0 +1,7 @@
+REM Creating a folder to contain the makefiles
+cd /D F:/redesigned-happiness
+cd /D F:/tools
+REM Building makefiles for all OS, in the folder makefiles of the project.
+REM They will all have the name make and their extension will be the OS
+REM they are built for
+cbp2make -in F:/redesigned-happiness/project.cbp -unix -out F:/redesigned-happiness/Makefile
