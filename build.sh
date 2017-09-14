a920652dd838079e1fe2ab6b3e4651a988bb77fc
Loodoor <folaefolc@outlook.fr>
2017-09-14 22:13:55 +0200

    update

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 echo "Environment: `uname -a`"
 echo "Compiler: `$CXX --version`"
 
+echo "make all"
 make || exit 1
+echo "------------------------------------"
+echo "make tests"
 make tests || exit 1
